6765b2c68e24069f166547e9d786b1421dc3af9a
Lewis Taylor <lewis@dangerfarms.com>
2016-05-18 12:18:17 +0100

    Make `to` expected data contain email only

--- tests/test_sendgrid_backend.py
+++ tests/test_sendgrid_backend.py
@@ -432,7 +432,7 @@ class SendGridBackendAnymailFeatureTests(SendGridBackendMockAPITestCase):
         data = self.get_api_call_data()
         smtpapi = self.get_smtpapi()
         self.assertEqual(data['toname'], [' ', 'Bob'])
-        self.assertEqual(data['to'], ['alice@example.com', 'Bob <bob@example.com>'])
+        self.assertEqual(data['to'], ['alice@example.com', 'bob@example.com'])
         self.assertEqual(smtpapi['to'], ['alice@example.com', 'Bob <bob@example.com>'])
         self.assertEqual(smtpapi['sub'], {
             ':name': ["Alice", "Bob"],
