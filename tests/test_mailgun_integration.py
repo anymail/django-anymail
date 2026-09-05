8ce3acac0f7d8f113dfc302bf2936a21b899957c
Mike Edmunds <medmunds@gmail.com>
2026-09-05 15:55:31 -0700

    Mailgun: Stop polling event API in integration tests
    Mailgun integration tests were hitting rate limits polling their events
    API. We don't do that for any other ESP, so just eliminate that part of
    the tests.
    
    (Should eventually replace with end-to-end delivery and tracking webhook
    testing, for all ESPs, but that's a larger project.)

--- tests/test_mailgun_integration.py
+++ tests/test_mailgun_integration.py
@@ -1,11 +1,8 @@
-import logging
 import os
 import unittest
 from datetime import datetime, timedelta
 from email.utils import formataddr
-from time import sleep
 
-import requests
 from django.test import SimpleTestCase, tag
 
 from anymail.exceptions import AnymailAPIError
@@ -56,50 +53,6 @@ class MailgunBackendIntegrationTests(AnymailTestMixin, SimpleTestCase):
         )
         self.message.attach_alternative("<p>HTML content</p>", "text/html")
 
-    def fetch_mailgun_events(
-        self, message_id, event=None, initial_delay=2, retry_delay=2, max_retries=5
-    ):
-        """Return list of Mailgun events related to message_id"""
-        url = "https://api.mailgun.net/v3/%s/events" % ANYMAIL_TEST_MAILGUN_DOMAIN
-        auth = ("api", ANYMAIL_TEST_MAILGUN_API_KEY)
-
-        # Despite the docs, Mailgun's events API actually expects the message-id
-        # without the <...> brackets (so, not exactly "as returned by the messages API")
-        # https://documentation.mailgun.com/api-events.html#filter-field
-        params = {"message-id": message_id[1:-1]}  # strip <...>
-        if event is not None:
-            params["event"] = event
-
-        # It can take a few seconds for the events to show up
-        # in Mailgun's logs, so retry a few times if necessary:
-        sleep(initial_delay)
-        response = None
-        for retry in range(max_retries):
-            if retry > 0:
-                sleep(retry_delay)
-            response = requests.get(url, auth=auth, params=params)
-            if 200 == response.status_code:
-                items = response.json()["items"]
-                if len(items) > 0:
-                    return items
-                # else no matching events found yet, so try again after delay
-            elif 500 <= response.status_code < 600:
-                # server error (hopefully transient); try again after delay
-                pass
-            elif 403 == response.status_code:
-                # "forbidden": this may be related to API throttling;
-                # try again after delay
-                pass
-            else:
-                response.raise_for_status()
-        # Max retries exceeded:
-        if response is not None and 200 != response.status_code:
-            logging.warning(
-                "Ignoring Mailgun events API error %d:\n%s"
-                % (response.status_code, response.text)
-            )
-        return None
-
     def test_simple_send(self):
         # Example of getting the Mailgun send status and message id from the message
         sent_count = self.message.send()
@@ -119,7 +72,6 @@ class MailgunBackendIntegrationTests(AnymailTestMixin, SimpleTestCase):
 
     def test_all_options(self):
         send_at = datetime.now().replace(microsecond=0) + timedelta(minutes=2)
-        send_at_timestamp = send_at.timestamp()
         from_email = formataddr(("Test From, with comma", self.from_email))
         message = AnymailMessage(
             subject="Anymail Mailgun all-options integration test",
@@ -147,63 +99,18 @@ class MailgunBackendIntegrationTests(AnymailTestMixin, SimpleTestCase):
         )
 
         message.send()
-        # Mailgun always queues:
         self.assertEqual(message.anymail_status.status, {"queued"})
-        message_id = message.anymail_status.message_id
-
-        events = self.fetch_mailgun_events(message_id, event="accepted")
-        if events is None:
-            self.skipTest(
-                "No Mailgun 'accepted' event after 30sec -- can't complete this test"
-            )
-            return
-
-        event = events.pop()
-        # don't care about order:
-        self.assertCountEqual(event["tags"], ["tag 1", "tag 2"])
-        # all metadata values become strings:
         self.assertEqual(
-            event["user-variables"], {"meta1": "simple string", "meta2": "2"}
-        )
-
-        self.assertEqual(event["message"]["scheduled-for"], send_at_timestamp)
-        self.assertIn(
-            event["recipient"],
-            [
-                "test+to1@anymail.dev",
-                "test+to2@anymail.dev",
-                "test+cc1@anymail.dev",
-                "test+cc2@anymail.dev",
-                "test+bcc1@anymail.dev",
-                "test+bcc2@anymail.dev",
-            ],
-        )
-
-        headers = event["message"]["headers"]
-        self.assertEqual(headers["from"], from_email)
-        self.assertEqual(
-            headers["to"], "test+to1@anymail.dev, Recipient 2 <test+to2@anymail.dev>"
+            message.anymail_status.recipients["test+to1@anymail.dev"].status, "queued"
         )
         self.assertEqual(
-            headers["subject"], "Anymail Mailgun all-options integration test"
+            message.anymail_status.recipients["test+to2@anymail.dev"].status, "queued"
         )
-
-        attachments = event["message"]["attachments"]
-        if len(attachments) == 3:
-            # The inline attachment shouldn't be in the event message.attachments array,
-            # but sometimes is included for the accepted event (see #172)
-            inline_attachment = attachments.pop(0)
-            self.assertEqual(inline_attachment["filename"], cid)
-            self.assertEqual(inline_attachment["content-type"], "image/png")
-        self.assertEqual(len(attachments), 2)
-        self.assertEqual(attachments[0]["filename"], "attachment1.txt")
-        self.assertEqual(attachments[0]["content-type"], "text/plain")
-        self.assertEqual(attachments[1]["filename"], "vedhæftet fil.csv")
-        self.assertEqual(attachments[1]["content-type"], "text/csv")
-
-        # No other fields are verifiable from the event data.
-        # (We could try fetching the message from event["storage"]["url"]
-        # to verify content and other headers.)
+        # single message should have the same message_id for each recipient:
+        self.assertEqual(
+            message.anymail_status.recipients["test+to1@anymail.dev"].message_id,
+            message.anymail_status.recipients["test+to2@anymail.dev"].message_id,
+        )
 
     def test_per_recipient_options(self):
         message = AnymailMessage(
@@ -259,16 +166,6 @@ class MailgunBackendIntegrationTests(AnymailTestMixin, SimpleTestCase):
         message.send()
         recipient_status = message.anymail_status.recipients
         self.assertEqual(recipient_status["test+to1@anymail.dev"].status, "queued")
-
-    # As of Anymail 0.10, this test is no longer possible, because
-    # Anymail now raises AnymailInvalidAddress without even calling Mailgun
-    # def test_invalid_from(self):
-    #     self.message.from_email = 'webmaster'
-    #     with self.assertRaises(AnymailAPIError) as cm:
-    #         self.message.send()
-    #     err = cm.exception
-    #     self.assertEqual(err.status_code, 400)
-    #     self.assertIn("'from' parameter is not a valid address", str(err))
 
     @override_settings(
         MAILERS={
